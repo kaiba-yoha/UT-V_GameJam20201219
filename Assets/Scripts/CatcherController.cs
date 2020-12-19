a8ad34c0916b21cb9ed9503aaf58e79e0ad4085c
kaiba-yoha <kaibasyatyou11@gmail.com>
2020-12-19 18:24:45 +0900

    Implemented Controller Scripts and Manager.

--- Assets/Scripts/CatcherController.cs
+++ Assets/Scripts/CatcherController.cs
@@ -8,9 +8,12 @@ public class CatcherController : MonoBehaviour
     public Vector2Int SelectingPos = Vector2Int.zero;
 
     [SerializeField]
-    float InputDelay = 1;
+    float PlaceDelay=1;
+    [SerializeField]
+    float MoveDelay = 1;
 
-    Coroutine InputCoroutine;
+    Coroutine MoveInputCoroutine;
+    Coroutine PlaceCoroutine;
 
     [SerializeField]
     GameObject SelectionObj;
@@ -21,25 +24,48 @@ public class CatcherController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        InputCoroutine = StartCoroutine(ProcessInput());
+        
     }
 
-    IEnumerator ProcessInput()
+    public void BePlayable()
+    {
+        MoveInputCoroutine = StartCoroutine(ProcessMoveInput());
+        PlaceCoroutine = StartCoroutine(ProcessPlacement());
+    }
+
+    public void BeUnPlayable()
+    {
+        StopCoroutine(MoveInputCoroutine);
+        StopCoroutine(PlaceCoroutine);
+    }
+
+    IEnumerator ProcessPlacement()
     {
         while (true)
         {
             if (Input.GetButton("Placement"))
             {
-                PlaceCube();
+                if (PlaceCube())
+                    yield return new WaitForSeconds(PlaceDelay);
+                else
+                    yield return null;
             }
+            else
+                yield return null;
+        }
+    }
 
-            Vector2Int input = new Vector2Int(Mathf.RoundToInt(Input.GetAxis("2PHorizontal")), Mathf.RoundToInt(Input.GetAxis("2PVertical")));
-            if (input.magnitude > 0)
+    IEnumerator ProcessMoveInput()
+    {
+        while (true)
+        {
+            Vector2Int input = new Vector2Int((int)Mathf.Ceil(Input.GetAxis("2PHorizontal")), (int)Mathf.Ceil(Input.GetAxis("2PVertical")));
+            if (input.magnitude > 0&&StageManager.Instance.GridData.ContainsKey(SelectingPos+input))
             {
                 SelectingPos += input;
                 input = Vector2Int.zero;
                 UpdateSelection();
-                yield return new WaitForSeconds(InputDelay);
+                yield return new WaitForSeconds(MoveDelay);
             }
             else
                 yield return null;
@@ -47,13 +73,14 @@ public class CatcherController : MonoBehaviour
 
     }
 
-    void PlaceCube()
+    bool PlaceCube()
     {
         if (StageManager.Instance.GridData[SelectingPos] != null)
-            return;
+            return false;
 
         GameObject obj = Instantiate(BlockPrefab, StageManager.Instance.GetCenterPos3D(SelectingPos), Quaternion.identity);
         StageManager.Instance.Regist(SelectingPos, obj);
+        return true;
     }
 
     void UpdateSelection()
