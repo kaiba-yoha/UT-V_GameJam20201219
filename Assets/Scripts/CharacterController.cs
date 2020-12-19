522226ef7967e68814a5ab5e4d4adf03b55fd3c6
RyoOkazaki <ryo-lavieg@hotmail.co.jp>
2020-12-19 14:08:59 +0900

    とりあえず動く

---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class CharacterController : MonoBehaviour
+{
+
+    [SerializeField] float speed = 1.0f;
+    Vector3 currentPosition;
+    Animator animator;
+    Vector3 direction;
+
+    // Use this for initialization
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        currentPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("1PHorizontal");
+        float z = Input.GetAxis("1PVertical");
+
+        if(x != 0) {
+            direction = new Vector3(x,0,0);
+            //animator.CrossFadeInFixedTime("Run",0);
+            animator.SetBool("isRun",true);
+        }
+        else if(z != 0) {
+            direction = new Vector3(0,0,z);
+            //animator.CrossFadeInFixedTime("Run",0);
+            animator.SetBool("isRun",true);
+        } else if(x == 0 && z == 0) {
+            //animator.CrossFadeInFixedTime("Idle",0);
+            animator.SetBool("isRun",false);
+        }
+
+
+        float step = speed * Time.deltaTime;
+        //Vector3 direction = new Vector3(x,0,z);
+        transform.position += direction * step;
+
+        Vector3 diff = transform.position - currentPosition;
+        transform.rotation = Quaternion.LookRotation(diff);
+        currentPosition = transform.position;
+    }
+}
