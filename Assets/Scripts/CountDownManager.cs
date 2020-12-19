a04eaf8969737e7449fa88b98fcf5010bf7b71b2
RyoOkazaki <ryo-lavieg@hotmail.co.jp>
2020-12-19 18:27:06 +0900

    soundmanager作成 タイトル画面作成中

---
+++ Assets/Scripts/CountDownManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CountDownManager : MonoBehaviour
+{
+    [SerializeField] Text countdownText = null;
+    [SerializeField] Button startButton = null;
+    [SerializeField] GameObject titleScreen = null;
+
+    void Start()
+    {
+        startButton.onClick.AddListener(()=>StartCoroutine(CountDown()));
+    }
+
+    IEnumerator CountDown()
+    {
+        titleScreen.SetActive(false);
+
+        int count = 3;
+        countdownText.text = count.ToString();
+
+        while(count > 0) {
+            
+            yield return new WaitForSeconds(1);
+            countdownText.text = count.ToString();
+            count--;
+        }
+
+        countdownText.enabled = false;
+    }
+}
---
+++ Assets/SoundManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum SEID
+{
+    PlayStart,
+    PlayFinish,
+}
+
+public class SoundManager : MonoBehaviour
+{
+    [SerializeField] AudioSource bgmAudioSource;
+    [SerializeField] AudioSource seAudioSource;
+
+    [SerializeField] AudioClip[] naguruVoices;
+    [SerializeField] AudioClip brakeBlock;
+    [SerializeField] AudioClip playStartVoice;
+    [SerializeField] AudioClip playFinishVoice;
+
+
+    public void PlayBGM()
+    {
+        bgmAudioSource.Play();
+    }
+
+    public void StopBGM()
+    {
+        bgmAudioSource.Stop();
+    }
+
+    public void PlaySE(SEID seID)
+    {
+        switch(seID) {
+            case SEID.PlayStart:
+                seAudioSource.PlayOneShot(playStartVoice);
+                break;
+            case SEID.PlayFinish:
+                seAudioSource.PlayOneShot(playFinishVoice);
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void PlayBrakeBlockSE()
+    {
+        int i = Random.Range(0,naguruVoices.Length);
+        seAudioSource.PlayOneShot(naguruVoices[i]);
+
+        seAudioSource.PlayOneShot(brakeBlock);
+    }
+}
